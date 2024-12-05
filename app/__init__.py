c6317b67b9c4c229781f24d4328ed80d80f7edb7
Syed-Muhammad-Ahmer <syedmuhammadahmernaqvi@gmail.com>
2024-12-06 01:53:44 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ def create_app():
 
     app = Flask(__name__)
     app.config.from_object('config.Config')
+    ckeditor = CKEditor(app)
 
     # Initialize plugins
     db.init_app(app)
